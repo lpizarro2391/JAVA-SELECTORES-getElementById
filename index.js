826388e1ad4ced7059de0322637d0f74f073dfc5
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-03-19 10:35:59 -0600

    Se aplican sobre el objeto document, se debe definir una variable y hacerla igual al a la seleccion el selcector nos devolverá un objeto tipo dom para que realicemos las modificaciones necesarias.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//Variable
 
+
+var nombre;
+var apellido;
+var email;
+var usuario;
+var password;
+var boton;
+
+
+//asignación
+
+nombre = document.getElementById("nombre");
+apellido = document.getElementById("apellido");
+email = document.getElementById("email");
+usuario= document.getElementById("usuario");
+password= document.getElementById("password");
+botton = document.getElementById("btn-guardar");
+
+//mostrar el objeto seleccionado
+
+console.log(password);
